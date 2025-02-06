598cc6fdb5a6ae8c7adbb957c2fb4fab1bd982d6
Nola Gerold <gingerwitch64@gmail.com>
2025-02-05 20:33:17 -0800

    I think I'm going to undo this config stuff but I'll try anyhow

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,17 +4,64 @@ use axum::{
     Json, Router,
 };
 use rand::{rngs::StdRng, RngCore, SeedableRng};
+use redis::Commands;
 use serde::{Deserialize, Serialize};
+use std::fs;
+
+pub const PREFIX_LOG: &'static str = "[LOG]";
+pub const PREFIX_ERROR: &'static str = "[ERROR]";
+
+#[derive(Deserialize)]
+struct Config {
+    utter_server: ServerConfig,
+    redis: RedisConfig,
+}
+
+#[derive(Deserialize)]
+struct ServerConfig {
+    ip: Option<String>,
+    port: Option<u16>,
+}
+
+#[derive(Deserialize)]
+struct RedisConfig {
+    hostname: Option<String>,
+    port: Option<u16>,
+    username: Option<String>,
+    password: Option<String>,
+}
+
+fn load_config(file_path: String) -> Config {
+    let config_contents: String = fs::read_to_string(file_path.clone()).expect(
+        format!(
+            "{} Could not read config file from: {}",
+            PREFIX_ERROR, file_path
+        )
+        .as_str(),
+    );
+    let mut config: Config = toml::from_str(config_contents.as_str()).unwrap();
+    config
+}
 
 #[tokio::main]
 async fn main() {
+    let config = load_config("./utter.server.config.toml".to_string());
     let app = Router::new()
         .route("/hello_world", get(hello_world))
         // `POST /users` goes to `create_user`
         .route("/users", post(create_user));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(
+        format!(
+            "{}:{}",
+            config.utter_server.ip.unwrap_or("0.0.0.0".to_string()),
+            config.utter_server.port.unwrap_or(3000)
+        )
+        .as_str(),
+    )
+    .await
+    .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -38,6 +85,15 @@ async fn create_user(
         username: payload.username,
         password: payload.password,
     };
+
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_connection().unwrap();
+    // throw away the result, just make sure it does not fail
+    let _: () = con.set("my_key", 42).unwrap();
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    let _res: redis::RedisResult<isize> = con.get("my_key");
 
     // this will be converted into a JSON response
     // with a status code of `201 Created`
