8bd182685609ca79d98a41bea2f2f74f17a37d18
Nola Gerold <gingerwitch64@gmail.com>
2025-02-08 15:47:55 -0800

    going to use compile-time parameters for this project, may change later

--- server/src/main.rs
+++ server/src/main.rs
@@ -15,43 +15,8 @@ pub const PREFIX_ERROR: &'static str = "[ERROR]";
 
 mod config;
 
-/*
-#[derive(Deserialize)]
-struct Config {
-    utter_server: ServerConfig,
-    redis: RedisConfig,
-}
-
-#[derive(Deserialize)]
-struct ServerConfig {
-    ip: Option<String>,
-    port: Option<u16>,
-}
-
-#[derive(Deserialize)]
-struct RedisConfig {
-    hostname: Option<String>,
-    port: Option<u16>,
-    username: Option<String>,
-    password: Option<String>,
-}
-
-fn load_config(file_path: String) -> Config {
-    let config_contents: String = fs::read_to_string(file_path.clone()).expect(
-        format!(
-            "{} Could not read config file from: {}",
-            PREFIX_ERROR, file_path
-        )
-        .as_str(),
-    );
-    let mut config: Config = toml::from_str(config_contents.as_str()).unwrap();
-    config
-}
-*/
-
 #[tokio::main]
 async fn main() {
-    //let config = load_config("./utter.server.config.toml".to_string());
     let app = Router::new()
         .route("/hello_world", get(hello_world))
         // `POST /users` goes to `create_user`
