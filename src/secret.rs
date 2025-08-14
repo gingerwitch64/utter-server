381184faa1a8a3c9c54db481e857d960fd566278
Nola Gerold <redpendrew@gmail.com>
2025-08-13 18:18:49 -0700

    example secret.rs

---
+++ src/secret.rs
@@ -0,0 +1,6 @@
+pub const DB_URL: &'static str =
+    "mysql://user:password@127.0.0.1:3306/example_database";
+
+pub const JWT_SECRET:
+    &[u8] =
+    b"SomeLongPreferablyRandomlyGeneratedStringToKeepYourStuffSafe";
